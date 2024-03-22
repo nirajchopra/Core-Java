16b8934b3a8022faa967458121ec26d627835c20
nirajchopra <nirajch0700@gmai.com>
2024-03-22 10:04:29 +0530

    22/03/2024

---
+++ JavaBasic/src/com/rays/Input.java
@@ -0,0 +1,16 @@
+package com.rays;
+
+import java.util.Scanner;
+
+public class Input {
+	public static void main(String args[]) {
+		//Input
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int sum = a + b;
+		System.out.println("number: "+sum);
+		
+	}
+
+}
