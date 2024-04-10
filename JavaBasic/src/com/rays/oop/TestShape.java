ca475f78a4759ff03e24bf681c748c6b110037c0
nirajchopra <nirajch0700@gmai.com>
2024-04-10 15:45:53 +0530

    done

--- JavaBasic/src/com/rays/oop/TestShape.java
+++ JavaBasic/src/com/rays/oop/TestShape.java
@@ -1,18 +1,15 @@
 package com.rays.oop;
 
 public class TestShape {
-	public static void main(String[] args) {
-		
-		Shape s = new Shape();
-		
-		s.setColor("Purple");
-		s.setBordorWidth(23);
-		
-		int bordorW = s.getBordorWidth();
-		String Color  = s.getColor();
-		
-		System.out.println(bordorW);
-		System.out.println(Color);
-	}
-
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        
+        // Set shape details
+        shape.setColor("Red");
+        shape.setBorderWidth(2);
+        
+        // Get and print shape details
+        System.out.println("Color: " + shape.getColor());
+        System.out.println("Border Width: " + shape.getBorderWidth());
+    }
 }
