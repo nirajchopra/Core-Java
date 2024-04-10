fed03b2983b856321ca8967fb9fe628db897d2f0
nirajchopra <nirajch0700@gmai.com>
2024-04-10 15:27:41 +0530

    done

--- JavaBasic/src/com/rays/oop/TestPerson.java
+++ JavaBasic/src/com/rays/oop/TestPerson.java
@@ -1,35 +1,33 @@
 package com.rays.oop;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.rays.SimpleDateFormat;
-
 public class TestPerson {
-	public static void main(String[] args) {
-		
-	
-	Person p = new Person();
-	SimpleDateFormat sdf = new SimpleDateFormat();
-	
-    String name = p.getName();
-    String address = p.getAddress();
-    Date dob =  p.getDob();
-    int AVG_AGE = p.getAvgAge();
-    
-    String s = "13-12-2002";
-    Date d = (Date) sdf.parse(s);
-    p.setName("Niraj Chopra");
-    System.out.println(p.getName());
-    p.setDob("13-12-2002");
-    System.out.println(p.setDob());
-    p.setAddress("Rahadkot");
-    System.out.println(p.setAddress());
+    public static void main(String[] args) {
+        Person p = new Person();
 
-    
-	
-	
-	}
-  
-	
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 
+        String name = p.getName();
+        String address = p.getAddress();
+        Date dob =  p.getDob();
+        int AVG_AGE = p.getAvgAge();
+
+        String s = "13-12-2002";
+        Date d = null;
+        try {
+            d = sdf.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        p.setName("Niraj Chopra");
+        System.out.println(p.getName());
+        p.setDob(d);
+        System.out.println(p.getDob());
+        p.setAddress("Rahadkot");
+        System.out.println(p.getAddress());
+    }
 }
