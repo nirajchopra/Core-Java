8fb0cb23cfa19f7d53a02fd8453251dc0ed8c30a
nirajchopra <nirajch0700@gmai.com>
2024-04-23 13:56:28 +0530

    done

---
+++ JavaBasic/src/com/rays/oop/Shallow/Account.java
@@ -0,0 +1,12 @@
+package com.rays.oop.Shallow;
+
+public class Account implements Cloneable {
+
+	public int balance;
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+}
