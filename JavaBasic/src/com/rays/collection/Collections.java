0e4071d196ee65996b127c382786e79d27e98900
nirajchopra <nirajch0700@gmai.com>
2024-04-27 15:41:56 +0530

    done

---
+++ JavaBasic/src/com/rays/collection/Collections.java
@@ -0,0 +1,32 @@
+package com.rays.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Collections {
+	
+	public static void main(String[] args) {
+		
+		Collection co = new ArrayList();
+		
+		co.add("bura mat dekho");
+		co.add("bura mat suno");
+		co.add("bura mat bolo");
+		
+		System.out.println("Legth of Size: " + co.size());
+		
+		for (Object Obj1 : co) {
+			System.out.println(Obj1);
+			
+		}
+		Object[] oArray = co.toArray();
+		
+		for(Object Obj : oArray) {
+			String s = (String) Obj;
+			System.out.println(s);
+		}
+		
+		
+	}
+
+}
