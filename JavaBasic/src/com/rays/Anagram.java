47c05de6673ccdefd56c3524d32c4cc68a6704db
nirajchopra <nirajch0700@gmai.com>
2024-05-05 00:36:37 +0530

    done

--- JavaBasic/src/com/rays/Anagram.java
+++ JavaBasic/src/com/rays/Anagram.java
@@ -18,34 +18,35 @@ public class Anagram{
 		char[] ch = str1.toCharArray();
 		char[] ch1 = str2.toCharArray();
 		
-		for (int i=0; i<ch.length; i++) {
-			for (int j=i+1; j<ch.length; j++) {
-				if(ch[i]>ch[j]) {
-				str=ch[i];
-				ch[i]=ch[j];
-				ch[j]=str;
-			}
-			}
-		}
-				for (int a=0; a<ch1.length; a++) {
-					for (int b=a+1; b<ch1.length; b++) {
-						if(ch1[a]>ch1[b]) {
-						str=ch1[a];
-						ch1[a]=ch1[b];
-						ch1[b]=str;
-			
-		}
-		
-		
-	}
+		for(int i = 0; i < ch.length; i++) {
+			for(int j = 0; j < ch.length; j++) {
+				if (ch[i]>ch[j]) {
+					str = ch[i];
+					ch[i] = ch[j];
+					ch[j] = str;
 					
-}
-				boolean result = Arrays.equals(ch, ch1);
-				if(result == true) {
-					System.out.println("Anagram");
-				} else {
-					System.out.println("Not Anagram");
 				}
 			}
+		}
+		for(int a = 0; a < ch1.length; a++) {
+			for(int b = 0; b < ch1.length; b++) {
+				if (ch1[a]>ch1[b]) {
+					str = ch1[a];
+					ch1[a] = ch[b];
+					ch[b] = str;
+					
+				}
+			}
+		}
+		
+		boolean result = Arrays.equals(ch, ch1);
+		if (result == true) {
+			System.out.println("Anagram");
+			
+		} else {
+			System.out.println("Not Anagarm");
+
+		}
+	}
 
 }
